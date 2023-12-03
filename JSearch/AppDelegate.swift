c5e3c63b3e0ca69e68c6b3be766668c6a29c4c55
Joao Pedro Fabiano Franco <joao.franco@deliveryhero.com>
2023-12-03 16:51:44 +0100

    create project and workspace

---
+++ JSearch/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  JSearch
+//
+//  Created by João Pedro Fabiano Franco on 03.12.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    startApp()
+
+    return true
+  }
+
+  private func startApp() {
+    let viewController = UIViewController()
+    viewController.view.backgroundColor = .blue
+
+    let navigationController = UINavigationController(rootViewController: viewController)
+    window?.rootViewController = navigationController
+    window?.makeKeyAndVisible()
+  }
+}
+
+
+
